afcb227a814cb2fd199864e160a8fd6be5acfecc
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-21 22:18:19 +0530

    Changes are pushed to repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,67 @@
-import React from 'react'
-import Header from './Components/Header'
-import './App.css'
+import React, { createContext, useContext, useReducer } from "react";
+import Header from "./Components/Header";
+import "./App.css";
 import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
-import TopHeadLine from './Components/TopHeadLine';
-import LeftSideBar from './Components/LeftSideBar';
-import PlayList from './Components/PlayList';
-import { Route, Routes } from 'react-router-dom';
-import Watch from './Components/Watch';
+import TopHeadLine from "./Components/TopHeadLine";
+import LeftSideBar from "./Components/LeftSideBar";
+import PlayList from "./Components/PlayList";
+import { Route, Routes, Router } from "react-router-dom";
+import Watch from "./Components/Watch";
 
-function App() {
+// Created context
+const searchContext = createContext(null);
+const dispatchContext = createContext(null);
+
+export function App() {
+  const [search, dispatch] = useReducer(searchReducer, "all");
   return (
-<>
-<div className="main-container">
-<Header/>
-<TopHeadLine/>
-<LeftSideBar/>
-<PlayList/>
-</div>
-</>  )
+    <>
+      <div className="main-container">
+        <searchContext.Provider value={search}>
+          <dispatchContext.Provider value={dispatch}>
+             <Header />
+            <Routes>
+              <Route path="/" element={<TopHeadLine />} />
+              <Route path="/search/:searchTerm" element={<TopHeadLine />} />
+            </Routes>
+
+            <Routes>
+              <Route path="/" element={<LeftSideBar />} />
+            </Routes>
+
+            <Routes>
+              <Route path="/" element={<PlayList />} />
+              <Route path="/watch/:id" element={<Watch />} />
+              <Route path="/search/:searchTerm" element={<PlayList />} />
+            </Routes> 
+
+
+        
+          </dispatchContext.Provider>
+        </searchContext.Provider>
+      </div>
+    </>
+  );
 }
 
-export default App
+// custom hooks
+
+export const useSearch = () => {
+  return useContext(searchContext);
+};
+
+export const useDispatch = () => {
+  return useContext(dispatchContext);
+};
+
+// created reducer function
+function searchReducer(initialVal, action) {
+  switch (action.type) {
+    case "change": {
+      return action.data;
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
